196a0f389dd6251ac2eb0b74f0b775b130d1dcda
Diiar <aitmarowd@gmail.com>
2019-12-02 15:20:51 +0600

    дз №68. Бонус №2.

--- source/webapp/static/js/main.js
+++ source/webapp/static/js/main.js
@@ -6,24 +6,23 @@ $(document).ready(function () {
     <p class="text-center font-italic" style="font-size: 40px">Calculate</p>
     <p>Введите А <input name="a" id="a"></p>
     <p>Введите B <input name="b" id="b"></p>
-    <a class="btn btn-success" onclick="add()">+</a>
-    <a class="btn btn-success" onclick="substract()">-</a>
-    <a class="btn btn-success" onclick="multiply()">*</a>
-    <a class="btn btn-success" onclick="divide()">/</a>
+    <a class="btn btn-success" data-id = 'add/' onclick="math(this)">+</a>
+    <a class="btn btn-success" data-id = 'substract/' onclick="math(this)">-</a>
+    <a class="btn btn-success" data-id = 'multiply/' onclick="math(this)">*</a>
+    <a class="btn btn-success" data-id = 'divide/' onclick="math(this)">/</a>
          <p>Ответ:</p>
     <div class="answer">
-
     </div>
     </form>`
     )
 });
 
-function add() {
+function math(elem) {
     a = document.getElementById('a');
     b = document.getElementById('b');
     let ajax = $.ajax({
       type: "POST",
-      url: "http://localhost:8000/add/",
+      url: "http://localhost:8000/" + $(elem).data('id'),
       data: JSON.stringify({
           'A' : a.value,
           'B' : b.value
@@ -42,75 +41,4 @@ function add() {
     });
 }
 
-function substract() {
-    a = document.getElementById('a');
-    b = document.getElementById('b');
-    let ajax = $.ajax({
-      type: "POST",
-      url: "http://localhost:8000/substract/",
-      data: JSON.stringify({
-          'A' : a.value,
-          'B' : b.value
-      }),
-      dataType:'json',
-      contentType:'json',
-      success: function (data) {
-        answer = $('.answer')
-          answer.html(`<p> ${data.answer}  </p>`)
-      },
-      error: function (data) {
-        answer = $('.answer')
-          answer.html(`<p style="color: red"> ${data.responseJSON.error}  </p>`)
-      },
-
-    });
-}
-
-function multiply() {
-    a = document.getElementById('a');
-    b = document.getElementById('b');
-    let ajax = $.ajax({
-      type: "POST",
-      url: "http://localhost:8000/multiply/",
-      data: JSON.stringify({
-          'A' : a.value,
-          'B' : b.value
-      }),
-      dataType:'json',
-      contentType:'json',
-      success: function (data) {
-        answer = $('.answer')
-          answer.html(`<p> ${data.answer}  </p>`)
-      },
-      error: function (data) {
-        answer = $('.answer')
-          answer.html(`<p style="color: red"> ${data.responseJSON.error}  </p>`)
-      },
-
-    });
-}
-
-function divide() {
-    a = document.getElementById('a');
-    b = document.getElementById('b');
-    let ajax = $.ajax({
-      type: "POST",
-      url: "http://localhost:8000/divide/",
-      data: JSON.stringify({
-          'A' : a.value,
-          'B' : b.value
-      }),
-      dataType:'json',
-      contentType:'json',
-      success: function (data) {
-        answer = $('.answer')
-          answer.html(`<p> ${data.answer}  </p>`)
-      },
-      error: function (data) {
-        answer = $('.answer')
-          answer.html(`<p style="color: red"> ${data.responseJSON.error}  </p>`)
-      },
-
-    });
-}
 
